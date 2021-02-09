65d2fece211c665398544d85fadb949c2c34c397
General Orange <erik.17021995@gmail.com>
2021-02-09 21:47:55 +0100

    First night coding

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta description="Bij verzekerdtewerk zorgen we ervoor dat uw opdracht profesioneel wordt opgepakt door een van onze vakmannen!">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
@@ -14,65 +14,87 @@
 <body>
     <section class="mainContent">
         <div class="mainContent__inner">
-            <h1>Meldt je nu aan bij verzekerdtewerk.nl!</h1>
-            <p>Wij van verzekerd te werk helpen jou de klus probleemloos te realiseren.</p>
-            <a class="btn btn-success" href="#plaats-klus">Plaats een klus</a>
-
+            <div class="row">
+                <div class="col-12">
+                    <h1>Meldt je nu aan bij verzekerdtewerk.nl!</h1>
+                    <p>Wij van verzekerd te werk helpen jou de klus probleemloos te realiseren.</p>
+                </div>
+            </div>
 
             <!-- Begin Mailchimp Signup Form -->
-            <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-            <style type="text/css">
-                #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-                /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-                We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-            </style>
-            <div id="mc_embed_signup">
-            <form action="https://verzekerdtewerk.us7.list-manage.com/subscribe/post?u=c072e8ad549fde0321bbc053f&amp;id=1e66aaa7cd" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                <div id="mc_embed_signup_scroll">
-                
-            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-            <div class="mc-field-group">
-                <label for="mce-EMAIL">Email  <span class="asterisk">*</span>
-            </label>
-                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-            </div>
-            <div class="mc-field-group">
-                <label for="mce-FNAME">Naam  <span class="asterisk">*</span>
-            </label>
-                <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-            </div>
-            <div class="mc-field-group size1of2">
-                <label for="mce-PHONE">Telefoon </label>
-                <div class="phonefield phonefield-us">
-                    (<span class="phonearea"><input class="phonepart " pattern="[0-9]*" id="mce-PHONE-area" name="PHONE[area]" maxlength="3" size="3" value="" type="text"></span>)
-                    <span class="phonedetail1"><input class="phonepart " pattern="[0-9]*" id="mce-PHONE-detail1" name="PHONE[detail1]" maxlength="3" size="3" value="" type="text"></span> - 
-                    <span class="phonedetail2"><input class="phonepart " pattern="[0-9]*" id="mce-PHONE-detail2" name="PHONE[detail2]" maxlength="4" size="4" value="" type="text"></span>
-                    <span class="small-meta nowrap">(###) ###-####</span>
-                </div>
-            </div><div class="mc-field-group">
-                <label for="mce-MMERGE2">Klant/Aannemer  <span class="asterisk">*</span>
-            </label>
-                <select name="MMERGE2" class="required" id="mce-MMERGE2">
-                <option value=""></option>
-                <option value="Ik wil een klus plaatsen">Ik wil een klus plaatsen</option>
-            <option value="Ik wil klussen">Ik wil klussen</option>
+            <div id="mc_embed_signup" class="basicForm">
+                <form action="https://verzekerdtewerk.us7.list-manage.com/subscribe/post?u=c072e8ad549fde0321bbc053f&amp;id=1e66aaa7cd" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll" class="basicForm__inner">
+                        <div class="row">
+                            <div class="col-12 col-lg-6">
+                                <div class="mb-3">
+                                    <label for="mce-MMERGE2" class="form-label">Klant / Aannemer  <span class="asterisk">*</span></label>
+                                    <select name="MMERGE2" class="required form-select" id="mce-MMERGE2">
+                                        <option value=""></option>
+                                        <option value="Ik wil een klus plaatsen">Ik wil een klus plaatsen</option>
+                                        <option value="Ik wil klussen">Ik wil klussen</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6">
+                                <div class="mb-3">
+                                    <label for="mce-EMAIL" class="form-label">Email  <span class="asterisk">*</span></label>
+                                    <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
+                                </div>
+                            </div>
+                            
+                            <div class="col-12 col-lg-6">
+                                <div class="mb-3">
+                                    <label for="mce-FNAME" class="form-label">Naam  <span class="asterisk">*</span></label>
+                                    <input type="text" value="" name="FNAME" class="required form-control" id="mce-FNAME">
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6">
+                                <div class="mb-3">
+                                    <label for="mce-MMERGE3" class="form-label">Adres </label>
+                                    <input type="text" value="" name="MMERGE3" class="form-control" id="mce-MMERGE3">
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6">
+                                <div class="mb-3">
+                                    <label for="mce-PHONE" class="form-label">Telefoon </label>
+                                    <input type="number" name="PHONE" class="form-control" value="" id="mce-PHONE">
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6">
+                                <div class="mb-3">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" required>
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Ik ga akkoord met de <a href="./assets/documents/Algemene_voorwaarden_opdrachtgever.docx">algemene voorwaarden</a><span class="asterisk">*</span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6">
+                                <div class="indicates-required">
+                                    <span class="asterisk">*</span> is verplicht
+                                </div>
+                            </div>
 
-                </select>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                <input type="text" name="b_c072e8ad549fde0321bbc053f_1e66aaa7cd" tabindex="-1" value="">
+                            </div>
+
+                            <div class="col-12">
+                                <div class="clear">
+                                    <input type="submit" value="Meldt aan!" name="subscribe" id="mc-embedded-subscribe" class="btn btn-success">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div class="mc-field-group">
-                <label for="mce-MMERGE3">Adres </label>
-                <input type="text" value="" name="MMERGE3" class="" id="mce-MMERGE3">
-            </div>
-                <div id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c072e8ad549fde0321bbc053f_1e66aaa7cd" tabindex="-1" value=""></div>
-                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                </div>
-            </form>
-            </div>
-            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[4]='PHONE';ftypes[4]='phone';fnames[2]='MMERGE2';ftypes[2]='dropdown';fnames[3]='MMERGE3';ftypes[3]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='MMERGE2';ftypes[2]='dropdown';fnames[3]='MMERGE3';ftypes[3]='text';fnames[4]='PHONE';ftypes[4]='number';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
             <!--End mc_embed_signup-->
 
 
